20a9b174091d78a6ff64183305b9969611e339b7
shrikantingle205 <108973865+shrikantingle205@users.noreply.github.com>
2022-07-13 12:54:45 +0530

    Create DAY_2_PROG_1.c

---
+++ DAY_2/DAY_2_PROG_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    long int num;
+    int rem,sum=0;
+    printf("Enter the Mobile number\n");
+    scanf("%ld",&num);
+    loop:
+	if(num!=0)
+    {
+        rem=num%10;
+        sum=sum+rem;
+        num=num/10;
+goto loop;
+    }
+        printf("%ld",sum);
+}
