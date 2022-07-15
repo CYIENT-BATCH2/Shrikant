5bab511f0a3a378141192783b4407eaae463788c
shrikantingle205 <108973865+shrikantingle205@users.noreply.github.com>
2022-07-15 13:12:02 +0530

    Create DAY_4_(I)BITWISE OPERATOR_1.c

---
+++ DAY_4/DAY_4_(I)BITWISE OPERATOR_1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+    int pos,data,bit=7;
+    printf("Enter the Data\n");
+    scanf("%d",&data);
+    printf("Enter the Pos\n");
+    scanf("%d",&pos);
+    data=data|(1<<pos);
+    loop:
+    if(bit>=0)
+    {
+       printf("%d",(data>>bit)&1);
+        bit--;
+        goto loop;
+    }
+    printf("\n");
+    printf("Data= %d",data);
+}
