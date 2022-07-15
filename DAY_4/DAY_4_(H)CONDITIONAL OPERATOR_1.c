6f73152db7cfcb420115445e860c0e5e6843ad39
shrikantingle205 <108973865+shrikantingle205@users.noreply.github.com>
2022-07-15 09:41:51 +0530

    Create DAY_4_(H)CONDITIONAL OPERATOR_1.c

---
+++ DAY_4/DAY_4_(H)CONDITIONAL OPERATOR_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main()
+{
+int var1, var2, temp; 
+printf("Enter two intege\n");
+scanf("%d%d", &var1, &var2);
+(var1>var2)?printf("var1 is Bigger\n"):printf("var2 is Bigger\n");
+return 0;
+}
