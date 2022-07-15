1a3f1f16a68d66a32afb26b6800be286a5886cb9
shrikantingle205 <108973865+shrikantingle205@users.noreply.github.com>
2022-07-15 09:43:47 +0530

    Create DAY_4_(H)CONDITIONAL OPERATOR_3.c

---
+++ DAY_4/DAY_4_(H)CONDITIONAL OPERATOR_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main()
+{
+int var1, var2, temp; 
+printf("Enter intege\n");
+scanf("%d", &var1);
+(var1>0)?printf("Positive Number\n"):printf("Negative number\n");
+return 0;
+}
