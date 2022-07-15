7c368908b13e48bde13f044ba37f71fad9c7456c
shrikantingle205 <108973865+shrikantingle205@users.noreply.github.com>
2022-07-15 09:42:37 +0530

    Create DAY_4_(H)CONDITIONAL OPERATOR_2.c

---
+++ DAY_4/DAY_4_(H)CONDITIONAL OPERATOR_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main()
+{
+int var1, var2, temp; 
+printf("Enter intege\n");
+scanf("%d", &var1);
+(var1 % 2==0)?printf("Even number\n"):printf("Odd numner\n");
+return 0;
+}
