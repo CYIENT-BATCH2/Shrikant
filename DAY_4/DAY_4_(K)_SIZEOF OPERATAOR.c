14bf5c02d952559b4103d34c318002cea7c72f46
shrikantingle205 <108973865+shrikantingle205@users.noreply.github.com>
2022-07-15 17:20:28 +0530

    Create DAY_4_(K)_SIZEOF OPERATAOR.c

---
+++ DAY_4/DAY_4_(K)_SIZEOF OPERATAOR.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+   
+    
+    printf("char=%d\n",sizeof(char));
+     printf("int=%d\n",sizeof(int));
+      printf("float=%d\n",sizeof(float));
+       printf("double=%d",sizeof(double));
+    
+    
+    
+}
